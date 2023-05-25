94e27c6206fc2c943692b0b92f477e073d23eed5
Raymond Lu <raymondlu77@gmail.com>
2023-05-25 15:49:30 -0500

    set up hibernate project
    configure maven dependencies and downloaded needed tools

--- App.java
+++ App.java
@@ -1,11 +1,23 @@
 package com.ray.DemoHib;
 
-//comment
+import org.hibernate.SessionFactory;
+import org.hibernate.Session;
+import org.hibernate.cfg.Configuration;
 
 public class App 
 {
     public static void main( String[] args )
     {
         Alien ray = new Alien();
+        ray.setAid(100);
+        ray.setAname("bot");
+        ray.setColor("Green");
+        
+        Configuration con = new Configuration();
+        
+        SessionFactory sf = con.buildSessionFactory();
+        
+        Session session = sf.openSession();
+        session.save(ray);
     }
 }
